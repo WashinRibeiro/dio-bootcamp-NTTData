9b8e6a046b5ab7ad4e1fc189cf6a5b7a5fb316ef
Washington Ribeiro <washinuchiha@gmail.com>
2022-12-06 12:49:13 -0300

    bootcamp intermediate challenges

---
+++ Desafios de código/Desafios intermediários/05 - RemovendoLetrasDuplicadas.js
@@ -0,0 +1,16 @@
+const s = gets();
+
+let result = "";
+
+for (let index = 0; index < s.length; index++) {
+  const str = s[index];
+
+  if (result.includes(str)) continue;
+
+  while (str === result) {
+    result = result.slice(0, -1);
+  }
+  result += str;
+}
+
+print(result);
